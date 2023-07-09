39b045364d2439194d2158d8aa905529a285bf67
2ofael <66325118+2ofael@users.noreply.github.com>
2023-07-09 11:11:43 +0600

    first qna complete

--- CarrierPortal/Controllers/AnswerController.cs
+++ CarrierPortal/Controllers/AnswerController.cs
@@ -30,7 +30,7 @@ namespace CarrierPortal.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(AnswerViewModel answer)
         {
-            if (answer.Content!=null && answer.QuestionId!=null)
+            if (answer.Content != null && answer.QuestionId != null)
             {
                 Answer newAnswer = new Answer
                 {
@@ -57,7 +57,7 @@ namespace CarrierPortal.Controllers
             {
                 return NotFound();
             }
-            
+
 
             var answer = await _qnaRepository.GetAnswerByIdAsync(id);
 
@@ -68,93 +68,95 @@ namespace CarrierPortal.Controllers
 
             return View(answer);
         }
+
+
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var answer = await _qnaRepository.GetAnswerByIdAsync(id);
+
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new AnswerViewModel
+            {
+                Id = answer.Id,
+                Content = answer.Content
+            };
+
+            return View(viewModel);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, AnswerViewModel answer)
+        {
+            if (id != answer.Id)
+            {
+                return NotFound();
+            }
+
+            if (answer.Content != null && answer.Id != null)
+            {
+                
+                Answer existingAnswer = await _qnaRepository.GetAnswerByIdAsync(id);
+                if (existingAnswer == null)
+                {
+                    return NotFound();
+                }
+
+                existingAnswer.Content = answer.Content;
+
+                await _qnaRepository.UpdateAnswerAsync(existingAnswer);
+                return RedirectToAction("Details", "Answer", new { id = existingAnswer.Id });
+            }
+
+            return View(answer);
+        }
+
+        //// GET: Answer/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var answer = await _qnaRepository.GetAnswerByIdAsync(id);
+
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            return View(answer);
+        }
+
+        // POST: Answer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var answer = await _qnaRepository.GetAnswerByIdAsync(id);
+
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            string questionId = answer.QuestionId;
+
+            await _qnaRepository.DeleteAnswerAsync(answer);
+            return RedirectToAction("Details", "QnA", new { id = questionId });
+        }
+
     }
-
-    //// GET: Answer/Edit/5
-    //public async Task<IActionResult> Edit(string id)
-    //{
-    //    if (string.IsNullOrEmpty(id))
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    var answer = await _qnaRepository.GetAnswerByIdAsync(id);
-
-    //    if (answer == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    var viewModel = new AnswerViewModel
-    //    {
-    //        Id = answer.Id,
-    //        Content = answer.Content
-    //    };
-
-    //    return View(viewModel);
-    //}
-
-    //// POST: Answer/Edit/5
-    //[HttpPost]
-    //[ValidateAntiForgeryToken]
-    //public async Task<IActionResult> Edit(string id, AnswerViewModel answer)
-    //{
-    //    if (id != answer.Id)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    if (ModelState.IsValid)
-    //    {
-    //        Answer existingAnswer = await _qnaRepository.GetAnswerByIdAsync(id);
-    //        if (existingAnswer == null)
-    //        {
-    //            return NotFound();
-    //        }
-
-    //        existingAnswer.Content = answer.Content;
-
-    //        await _qnaRepository.UpdateAnswerAsync(existingAnswer);
-    //        return RedirectToAction("Details", "QnA", new { id = existingAnswer.QuestionId });
-    //    }
-
-    //    return View(answer);
-    //}
-
-    //// GET: Answer/Delete/5
-    //public async Task<IActionResult> Delete(string id)
-    //{
-    //    if (string.IsNullOrEmpty(id))
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    var answer = await _qnaRepository.GetAnswerByIdAsync(id);
-
-    //    if (answer == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    return View(answer);
-    //}
-
-    //// POST: Answer/Delete/5
-    //[HttpPost, ActionName("Delete")]
-    //[ValidateAntiForgeryToken]
-    //public async Task<IActionResult> DeleteConfirmed(string id)
-    //{
-    //    var answer = await _qnaRepository.GetAnswerByIdAsync(id);
-
-    //    if (answer == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    string questionId = answer.QuestionId;
-
-    //    await _qnaRepository.DeleteAnswerAsync(answer);
-    //    return RedirectToAction("Details", "QnA", new { id = questionId });
-    //}
-}
 }
